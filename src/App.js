b9cdcec17ac0970e6409b68b0e87143799c9739a
Niko Colom <shogun.endo@gmail.com>
2020-08-18 17:41:25 -0500

    Convert App.js to Class-basis

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      worldGrid: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Plantorio</p>
+        </header>
+        <div className="game">
+          <div className="game-notifications">
+            TODO (Warnings about wilting/rotting plants and the like)
+          </div>
+          <br/>
+          <div className="game-shop">
+            TODO (Where you'll buy new seeds, machines, and greenhouse expansions)
+          </div>
+          <br/>
+          <div className="game-world-grid">
+            TODO (The game itself!)
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
