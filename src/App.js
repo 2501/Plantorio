225063647f55e3540d696cf6bf51882bad09e877
Niko Colom <shogun.endo@gmail.com>
2020-08-18 18:27:07 -0500

    (Unstable) Attempt at grid interaction and cleanup. Pausing to focus on basic growing mechanics.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import './App.css';
 
+class WorldCell extends React.Component {
+  render() {
+    return (
+      <td
+        className="world-cell"
+        onClick={() => this.props.onClick(r,c)}
+      >
+        {this.props.content.name}: {this.props.content.status}
+      </td>
+    )
+  }
+}
+
+class WorldGrid extends React.Component {
+  renderSquare(r,c) {
+    return (
+      <WorldCell
+        content={{name: "Amaranth", status: "Healthy"}}
+        onClick={() => this.props.onClick(r,c)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <table>
+        {this.state.worldGrid.map(row => <tr key={row}> {
+          row.map(cell => <WorldCell content={{name: "Amaranth", status: "Healthy"}} onClick={() => this.props.onClick(r,c)} key={cell}/>)
+        } </tr>)}
+      </table>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
